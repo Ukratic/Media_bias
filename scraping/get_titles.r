d72e6092259dd55d69a3f28efb15a9c24c8bab10
Ukratic <94476800+Ukratic@users.noreply.github.com>
2022-11-03 11:18:45 +0100

    cleaning up

--- scraping/get_titles.r
+++ scraping/get_titles.r
@@ -18,7 +18,6 @@ valeurs_actuelles = read_html("https://www.valeursactuelles.com/")
 liberation = read_html("https://www.liberation.fr/")
 humanite = read_html("https://www.humanite.fr/")
 
-
 #Today's Date
 theday = Sys.Date()
 today = toString(theday)
@@ -28,82 +27,81 @@ clean_title = function(htmlString) {
   return(str_squish(gsub("<.*?>", "", htmlString)))
 }
 
-# Titles for Le Monde
+# Titles (articles) + title (main title) for Le Monde
 titles_lm = le_monde %>% html_elements(".article__title") %>% clean_title
 num_articles_lm = length(titles_lm)
+title_lm = le_monde %>% html_elements(".article__title-label") %>% clean_title
 
-# Titles for Le Figaro
+# Titles (articles) + title (main title) for Le Figaro
 titles_fig = le_figaro %>% html_elements(".fig-grid4__title , .fig-premium-mark, .fig-ensemble__link, .fig-profile__headline, .fig-ensemble__title--photo, .flive-loop__title") %>% clean_title
 num_articles_fig = length(titles_fig)
+title_fig = le_figaro %>% html_elements(".fig-profile--first .fig-premium-mark") %>% clean_title
 
-# Titles for CNews
+#Titles (articles) + title (main title) for CNews
 titles_cn = c_news %>% html_elements(".dm-ledirect-title , .dm-news-title, .dm-tag-title") %>% clean_title
 num_articles_cn = length(titles_cn)
+title_cn = c_news %>% html_elements(".dm-hp-full-alert-title") %>% clean_title
 
-# Titles for 20 Minutes
+# Titles (articles) + title (main title) for 20 Minutes
 titles_20min = vingt_minutes %>% html_elements(".teaser-title") %>% clean_title
 num_articles_20min = length(titles_20min)
+title_20min = vingt_minutes %>% html_elements(".preview-cover .teaser-title") %>% clean_title
 
-# Titles for Les Echos
+# Titles (articles) + title (main title) for Les Echos
 titles_le = les_echos %>% html_elements(".sc-4cuy4z-0") %>% clean_title
 num_articles_le = length(titles_le)
+title_le = les_echos %>% html_elements(".kXSjxr") %>% clean_title
 
-# Titles for France Info
+# Titles (articles) + title (main title) for France Info
 titles_fi = france_info %>% html_elements(".topic a , .col a, .col-right a, .col-2 li a, a.title") %>% clean_title
 num_articles_fi = length(titles_fi)
+title_fi = france_info %>% html_elements(".h1") %>% clean_title
 
-#Titles for Nouvel Obs
+# Titles (articles) + title (main title) for Nouvel Obs
 titles_no = nouvel_obs %>% html_elements(".article-box-content__title, .article-box-title") %>% clean_title
 num_articles_no = length(titles_no)
+title_no = nouvel_obs %>% html_elements(".article-box-title") %>% clean_title
 
-# Titles for La Croix
+# Titles (articles) + title (main title) for La Croix
 titles_lc = la_croix %>% html_elements("h3") %>% clean_title
 num_articles_lc = length(titles_lc)
+title_lc = la_croix %>% html_elements("h1") %>% clean_title
 
-# Titles for Marianne
-titles_ma = marianne %>% html_elements(".agora-article__title a , .thumbnail__link, .thumbnail__link, .thumbnail__link, .focus-block__item-link") %>% clean_title
+# Titles (articles) + title (main title) for Marianne
+titles_ma = marianne %>% html_elements(".agora-article__title a , .thumbnail__link, .focus-block__item-link") %>% clean_title
 num_articles_ma = length(titles_ma)
+title_ma = marianne %>% html_elements(".thumbnail__content--overlayed .thumbnail__link") %>% clean_title
 
-# Titles for BFM
+# Titles (articles) + title (main title) for BFM
 titles_bfm = bfmtv %>% html_elements(".title_une_item , .content_item_title") %>% clean_title
 num_articles_bfm = length(titles_bfm)
+title_bfm = bfmtv %>% html_elements(".title_une_item") %>% clean_title
 
-# Titles for L'Express
+# Titles (articles) + title (main title) for L'Express
 titles_lex = l_express %>% html_elements(".thumbnail__title") %>% clean_title
 num_articles_lex = length(titles_lex)
+title_lex = l_express %>% html_elements(".thumbnail--une .headline--lg") %>% clean_title
 
-# Titles for Le Point
+# Titles (articles) + title (main title) for Le Point
 titles_lp = le_point %>% html_elements(".list-view h2") %>% clean_title
 num_articles_lp = length(titles_lp)
+title_lp = le_point %>% html_elements(".art-full h2") %>% clean_title
 
-# Titles for Valeurs Actuelles
+# Titles (articles) + title (main title) for Valeurs Actuelles
 titles_va = valeurs_actuelles %>% html_elements(".h3 a") %>% clean_title
 num_articles_va = length(titles_va)
+title_va = valeurs_actuelles %>% html_elements(".card-post--featured .h3 a") %>% clean_title
 
-#Titles for Liberation
+# Titles (articles) + title (main title) for Liberation
 titles_lib = liberation %>% html_elements(".decoration_none .font_normal") %>% clean_title
 num_articles_lib = length(titles_lib)
+title_lib = liberation %>% html_elements(".font_normal") %>% clean_title
 
-#Titles for L'Humanite
+# Titles (articles) + title (main title) for L'Humanite
 titles_hum = humanite %>% html_elements(".view-mode-title a , .rubrique-monde .field-group-div .field-name-title a, .view-mode-media_small_title .field-group-div a, .view-mode-type_title_desc .field-name-title a, .view-mode-media_med_type_title_desc_light .field-name-title a, .view-mode-media_big_type_title_desc_tall .field-name-title a, .field-content a") %>% clean_title
 num_articles_hum = length(titles_hum)
-
-# Processing main titles 
-title_lm = le_monde %>% html_elements(".article__title-label") %>% clean_title
-title_fig = le_figaro %>% html_elements(".fig-profile--first .fig-premium-mark") %>% clean_title
-title_cn = c_news %>% html_elements(".dm-hp-full-alert-title") %>% clean_title
-title_20min = vingt_minutes %>% html_elements(".preview-cover .teaser-title") %>% clean_title
-title_le = les_echos %>% html_elements(".kXSjxr") %>% clean_title
-title_fi = france_info %>% html_elements(".h1") %>% clean_title
-title_no = nouvel_obs %>% html_elements(".article-box-title") %>% clean_title
-title_lc = la_croix %>% html_elements("h1") %>% clean_title
-title_ma = marianne %>% html_elements(".thumbnail__content--overlayed .thumbnail__link") %>% clean_title
-title_bfm = bfmtv %>% html_elements(".title_une_item") %>% clean_title
-title_lex = l_express %>% html_elements(".thumbnail--une .headline--lg") %>% clean_title
-title_lp = le_point %>% html_elements(".art-full h2") %>% clean_title
-title_va = valeurs_actuelles %>% html_elements(".card-post--featured .h3 a") %>% clean_title
-title_lib = liberation %>% html_elements(".font_normal") %>% clean_title
 title_hum = humanite %>% html_elements(".view-mode-media_big_type_title_desc_tall .field-name-title a") %>% clean_title
+
 
 #Writing main titles to csv
 headers = c(title_lm, title_fig, title_cn, title_20min, title_le, title_fi, title_no, title_lex, title_bfm, title_ma, title_lc, title_lp[1], title_va[1], title_lib[1], title_hum[1])
@@ -114,7 +112,6 @@ write.table(cbind(date_headers,name,headers), file="headers.csv", sep=";", dec="
 
 
 #Writing all titles to full csv
-
 name_lm = rep("Le Monde", num_articles_lm)
 name_fig = rep("Le Figaro", num_articles_fig)
 name_cn = rep("C News", num_articles_cn)
